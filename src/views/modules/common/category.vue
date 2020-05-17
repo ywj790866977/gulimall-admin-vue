8c7ebfaa927ade042e2e1132ca3afb1dab5359e1
yanwenjie <790866977@qq.com>
2020-05-17 23:12:20 +0800

    添加平台属性

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-tree :data="list" :props="defaultProps" node-key="catId" ref="menuTree"></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      expandedKeys: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    };
+  },
+  created() {
+    this.getMenus();
+  },
+  methods: {
+    async getMenus() {
+      const res = await this.$http({
+        url: this.$http.adornUrl("/product/category/list"),
+        method: "get"
+      });
+      this.list = res.data.data;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/modules/product/attrgroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <category></category>
+      </el-col>
+      <el-col :span="18">表格</el-col>
+    </el-row>
+  </div>
+  <!-- <div>1111</div> -->
+</template>
+
+<script>
+import Category from "../common/category";
+export default {
+  components: { Category },
+  data() {
+    return {};
+  },
+  created() {},
+
+  methods: {}
+};
+</script>
+
+<style>
+</style>
